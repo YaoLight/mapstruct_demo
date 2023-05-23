d07cd33c5fb21c5c3bfe7dc07a2ff2df43972373
YaoLight <2297110619@qq.com>
2023-05-23 17:14:21 +0800

    测试并修复转换赋值为null的问题

--- src/main/java/com/example/mapstruct/convert/UserConvert.java
+++ src/main/java/com/example/mapstruct/convert/UserConvert.java
@@ -11,8 +11,13 @@ import org.mapstruct.factory.Mappers;
  */
 @Mapper
 public interface UserConvert {
-
+    //使用Mappers工厂获取实现类
     UserConvert INSTANCE  = Mappers.getMapper(UserConvert.class);
 
-    UserEntity po2Entity(UserPo userPo);
+    /**
+     * 注意： Lombok引入顺序要在mapstruct之前，否则可能会导致转换赋值为null
+     * 参数对象: sourceObject
+     * 返回对象: TargetObject
+     */
+    UserEntity convert(UserPo userPo);
 }
--- src/main/java/com/example/mapstruct/convert/UserConvertTest.java
+++ src/main/java/com/example/mapstruct/convert/UserConvertTest.java
@@ -1,5 +1,7 @@
 package com.example.mapstruct.convert;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.date.StopWatch;
 import com.example.mapstruct.model.UserEntity;
 import com.example.mapstruct.model.UserPo;
 
@@ -26,8 +28,43 @@ public class UserConvertTest {
                 .age(18L)
                 .build();
         System.out.println("userPo: " + userPo);
-        UserEntity userEntity = UserConvert.INSTANCE.po2Entity(userPo);
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
+        UserEntity userEntity = UserConvert.INSTANCE.convert(userPo);
+
+        stopWatch.stop();
+        //耗时（毫秒）： 23
+        System.out.println("耗时（毫秒）： " + stopWatch.getTotalTimeMillis());
+
         System.out.println("userEntity: " + userEntity);
-        System.out.println("-----------testNormal-----ent------");
+        System.out.println("-----------testNormal-----end------");
+
+        testBeanUtil();
+    }
+
+    public static void testBeanUtil(){
+        System.out.println("--------------testBeanUtil--------start----------");
+        UserPo userPo = new UserPo();
+        userPo.setId(1L);
+        userPo.setGmtCreate(LocalDateTime.now());
+        userPo.setCreateTime(LocalDateTime.now());
+        userPo.setBuyerId(666L);
+        userPo.setAge(18L);
+        userPo.setUserNick("测试mapstruct");
+        userPo.setUserVerified("ok");
+        UserEntity userEntity = new UserEntity();
+
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
+        BeanUtil.copyProperties(userPo, userEntity);
+
+        stopWatch.stop();
+        //耗时（毫秒）： 386
+        System.out.println("耗时（毫秒）： " + stopWatch.getTotalTimeMillis());
+        System.out.println("userEntity: " + userEntity);
+
+        System.out.println("--------------testBeanUtil--------end----------");
     }
 }
